bdf6b92a2a1401beb44750c555530df795159c03
Andre Medeiros <me@andremedeiros.info>
2017-11-27 16:15:54 -0500

    Add more git stuff

--- zshrc.d/prompt.zsh
+++ zshrc.d/prompt.zsh
@@ -1,7 +1,21 @@
 : ${PROMPT_SYMBOL:="❯"}
+: ${PROMPT_GIT_AHEAD_SYMBOL:=""}
+: ${PROMPT_GIT_BEHIND_SYMBOL:=""}
 
 prompt_helper_git_branch() {
   (git name-rev --name-only --no-undefined --always HEAD || git symbolic-ref -q HEAD) 2> /dev/null
+}
+
+prompt_helper_git_ahead() {
+  git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' '
+}
+
+prompt_helper_git_behind() {
+  git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' '
+}
+
+prompt_helper_git_modifications() {
+  (git diff --quiet || git diff --cached --quiet || git ls-files --other --exclude-standard) 2> /dev/null
 }
 
 prompt_helper_path() {
@@ -16,6 +30,27 @@ prompt_precmd() {
   git_branch=$(prompt_helper_git_branch)
   if [ -n $git_branch ]; then
     PROMPT="${PROMPT} %F{"11"}%B${git_branch}%b%f"
+
+    git_modifications=$(prompt_helper_git_modifications)
+    if [ -n $git_modifications ]; then
+      PROMPT="${PROMPT}*"
+    fi
+
+    GIT_STATE=""
+
+    git_ahead=$(prompt_helper_git_ahead)
+    if [ "$git_ahead" -gt 0 ]; then
+      GIT_STATE="${GIT_STATE}${git_ahead}${PROMPT_GIT_AHEAD_SYMBOL}"
+    fi
+
+    git_behind=$(prompt_helper_git_behind)
+    if [ "$git_behind" -gt 0 ]; then
+      GIT_STATE="${GIT_STATE}${git_behind}${PROMPT_GIT_BEHIND_SYMBOL}"
+    fi
+
+    if [ -n $GIT_STATE ]; then
+      PROMPT="${PROMPT} ${GIT_STATE}"
+    fi
   fi
 
   # Prompt separator
