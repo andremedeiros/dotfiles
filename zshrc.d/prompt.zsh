6cf7b444b178f188709d676be2ebde1a41566a78
Andre Medeiros <me@andremedeiros.info>
2017-11-27 16:01:42 -0500

    Show git branch

--- zshrc.d/prompt.zsh
+++ zshrc.d/prompt.zsh
@@ -1,5 +1,9 @@
 : ${PROMPT_SYMBOL:="❯"}
 
+prompt_helper_git_branch() {
+  (git name-rev --name-only --no-undefined --always HEAD || git symbolic-ref -q HEAD) 2> /dev/null
+}
+
 prompt_helper_path() {
   print -Pn '%~'
 }
@@ -7,6 +11,12 @@ prompt_helper_path() {
 prompt_precmd() {
   # Path
   PROMPT="%F{"12"}`prompt_helper_path`%f"
+
+  # Git
+  git_branch=$(prompt_helper_git_branch)
+  if [ -n $git_branch ]; then
+    PROMPT="${PROMPT} %F{"11"}%B${git_branch}%b%f"
+  fi
 
   # Prompt separator
   PROMPT="${PROMPT} %F{"1"}%B${PROMPT_SYMBOL}%b%f "
