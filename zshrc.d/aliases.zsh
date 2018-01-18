f8f20c214b577a9589e08302b00380a420728689
Andre Medeiros <me@andremedeiros.info>
2018-01-18 15:23:48 -0500

    Add line numbers too

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -4,7 +4,7 @@ alias thisforthat='curl -s "http://itsthisforthat.com/api.php?text"'
 
 # serious
 alias git="hub"
-alias hl="highlight -O ansi"
+alias hl="highlight -O ansi -n"
 alias ia="open $1 -a /Applications/iA\ Writer.app"
 alias ls="gls --color=auto -F"
 alias servedir="ruby -run -ehttpd . -p8000"
