61d74a61b6b4dd655496d74d643b94fbfd563e28
Andre Medeiros <me@andremedeiros.info>
2018-10-19 10:24:44 -0400

    No time to commit.. My people need me!

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -4,8 +4,10 @@ function __servedir {
 }
 
 # humor
+alias commitmsg='curl -s "https://whatthecommit.com/index.txt"'
 alias dadjoke='curl -s "https://icanhazdadjoke.com"'
 alias thisforthat='curl -s "http://itsthisforthat.com/api.php?text"'
+alias git-vomit='git add -A; git commit -m `commitmsg`'
 
 # serious
 alias git="hub"
