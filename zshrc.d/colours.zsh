2ac49e6902c76c7960374375279c1666875f9165
Andre Medeiros <me@andremedeiros.info>
2019-02-17 14:34:34 -0500

    improve colours

--- zshrc.d/colours.zsh
+++ zshrc.d/colours.zsh
@@ -1,5 +1,10 @@
 function colours() {
-  for i in {0..255} ; do
-    printf "\x1b[38;5;${i}mcolour${i}\n"
+  for i in {0..15}; do
+    for j in {0..15}; do
+      k=$((j*16+i))
+
+      printf "\x1b[38;5;${k}m%-11s" "colour${k}"
+    done
+    printf "\n"
   done
 }
