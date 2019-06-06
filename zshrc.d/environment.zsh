bb37fcc3cdc6ae794791e442c5f492c3c99fe777
Andre Medeiros <me@andremedeiros.info>
2019-06-06 16:58:52 -0400

    more path things

--- zshrc.d/environment.zsh
+++ zshrc.d/environment.zsh
@@ -10,6 +10,7 @@ export VISUAL=nvim
 export EDITOR=nvim
 
 # add custom bin directories to path
+export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/opt/python/libexec/bin:$PATH
 export PATH=$PATH:~/.bin
 export PATH=$PATH:$GOPATH/bin
