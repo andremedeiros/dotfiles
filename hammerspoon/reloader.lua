32a5029a0a814ac4b297a539154976a7cc22d8a9
Andre Medeiros <me@andremedeiros.info>
2019-07-17 10:53:14 -0400

    alert

--- hammerspoon/reloader.lua
+++ hammerspoon/reloader.lua
@@ -3,7 +3,7 @@ local _M = {}
 local CONFIG_DIR = os.getenv("HOME") .. "/.hammerspoon/"
 
 function _M.setup(leader, key)
-  hs.hotkey.bind(leader, key, nil, hs.reload)
+  hs.hotkey.bind(leader, key, nil, hs.reload, hs.alert.show("hammerspoon config loaded"))
   hs.pathwatcher.new(CONFIG_DIR, hs.reload):start()
 end
 
