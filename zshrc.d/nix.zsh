71f13b55aad05dbd304f74ec558f69bb480a558a
Andre Medeiros <me@andremedeiros.info>
2020-06-24 16:41:10 -0400

    fix nix things better

--- zshrc.d/nix.zsh
+++ zshrc.d/nix.zsh
@@ -2,10 +2,3 @@
 if [ -f /nix/var/nix/profiles/default/etc/profile.d/nix.sh ]; then
   . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
 fi
-
-if [ ! -d "${HOME}/.nix-defexpr/channels/nixpkgs" ]; then
-  nix-channel --add https://channels.nixos.org/nixpkgs-20.03-darwin nixpkgs
-  nix-channel --update
-fi
-
-export NIX_PATH="nixpkgs=/Users/andremedeiros/.nix-defexpr/channels/nixpkgs"
