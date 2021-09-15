b63a8bef511f360de8fd21b76f9ff0427ed318e4
Andre Medeiros <hello@andre.cool>
2021-09-15 16:19:13 -0400

    move to starship

--- zshrc.d/prompt.zsh
+++ zshrc.d/prompt.zsh
@@ -1,26 +1,2 @@
-: "${PROMPT_SYMBOL:=>}"
-: "${PROMPT_UNSTAGED_SYMBOL:=*}"
-: "${PROMPT_STAGED_SYMBOL:=!}"
-
-prompt_precmd() {
-  vcs_info
-
-  PROMPT="%F{"12"}$(shrink_path -f)%f${vcs_info_msg_0_} %F{"1"}%B${PROMPT_SYMBOL}%b%f "
-}
-
-prompt_setup() {
-  autoload -Uz add-zsh-hook
-  autoload -Uz vcs_info
-
-  zstyle ':vcs_info:*' enable git
-  zstyle ':vcs_info:*' formats "%F{11} %b%f%F{1}%u%c%f"
-  zstyle ':vcs_info:*' check-for-changes true
-  zstyle ':vcs_info:*' unstagedstr "$PROMPT_UNSTAGED_SYMBOL"
-  zstyle ':vcs_info:*' stagedstr "$PROMPT_STAGED_SYMBOL"
-
-  setopt prompt_subst
-
-  add-zsh-hook precmd prompt_precmd
-}
-
-prompt_setup "$@"
+prompt="starship_prompt"
+eval "$(starship init zsh)"
