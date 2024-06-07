2c176ae126a327757dc0d9821ba89d3c4e70b050
Andre Medeiros <hello@andre.cool>
2024-06-06 20:27:51 -0400

    favor zellij over tmux

--- zshrc.d/m.zsh
+++ zshrc.d/m.zsh
@@ -1,33 +1,14 @@
 function __m {
   local sessname="${1:-$(basename $PWD)}"
 
-  # check tmuxinator
-  if [ -e "$HOME/.config/tmuxinator/$sessname.yml" ]; then
-    tmuxinator start "$sessname" --suppress-tmux-version-warning
-    return
-  fi
-
-  # check tmux sessions
-  if tmux has-session -t "$sessname" 2>/dev/null; then
+  # check zellij sessions
+  if zellij list-sessions --short | grep "$sessname" 2>/dev/null; then
     # session exists, lets attach
-    tmux attach -t "$sessname"
+    zellij attach "$sessname"
   else
     # session does not exist, start new one
-    tmux new -s "$sessname"
+    zellij -s "$sessname"
   fi
-}
-
-function __mk {
-  local sessname="${1:-$(basename $PWD)}"
-  sessions=$(tmux ls -F "#S" 2>/dev/null)
-
-  if [ -z "$sessions" ]; then
-    echo 'no active sessions'
-    return
-  fi
-
-  (grep -e "^$sessname$" <<< "$sessions" > /dev/null && tmux kill-session -t "$sessname") || echo 'session not found'
 }
 
 alias m='__m'
-alias mk='__mk'
